f153878b04ea206a86d4d736a3e8d4fb3ec6c5e6
Supermanu <manueltondeur@gmail.com>
2020-08-06 21:29:59 +0200

    Ensure packages are updated

--- recipe.sh
+++ recipe.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-sudo apt update && sudo apt install -y software-properties-common
+sudo apt update && sudo apt upgrade -y && sudo apt install -y software-properties-common
 sudo apt-add-repository --yes --update ppa:ansible/ansible
 sudo apt install -y ansible
 ansible-galaxy install -r requirements.yml
